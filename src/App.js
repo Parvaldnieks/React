c67c5b779e80135a6f9f7d18aa981fe6e9284c40
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-06-20 11:43:05 +0300

    :)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [zoodarzs, setZoodarzs] = useState([]);
+
+    useEffect(() => {
+
+      async function getData() {
+
+        const response = await fetch("animals.json");
+        const animals = await response.json();
+        setZoodarzs(animals);
+
+      }
+
+      getData();
+
+    }, []);
+
+    const renderZoodarzs = zoodarzs.map(nebrunurupucis => {
+      return (
+      <article>
+        <img src={nebrunurupucis["image"]} alt={nebrunurupucis.animal}> </img>
+        <div className="info">
+        <h2> {nebrunurupucis["animal"]} </h2>
+        <p> {nebrunurupucis["description"]} </p>
+        <ul>
+          {nebrunurupucis["answers"].map((answer) => {
+            <li> {answer} </li>
+          })}
+          </ul>
+        </div>
+      </article>
+
+      );
+    });
+
+
+return <main> { renderZoodarzs } </main>
+
 }
 
 export default App;
